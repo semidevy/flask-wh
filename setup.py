8fc9c9712dc0ed42ad604b87540e8cad426290a7
semidevy <seminar@devyan.de>
2019-09-09 11:41:33 +0200

    flask webhook

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='flask-wh-df',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Yannick',
+    author_email='',
+    description='', install_requires=['flask']
+)
